f621ba40a7ae23f02116bf7ac1dd94d6d9e1ae5b
stopsopa <grytysek@gmail.com>
2019-03-25 01:06:30 +0000

    rel

---
+++ cumulativeOffset.js
@@ -0,0 +1,20 @@
+
+/**
+ * https://stackoverflow.com/a/1480137/5560682
+ */
+var cumulativeOffset = function(element) {
+    var top = 0, left = 0;
+    do {
+        top += element.offsetTop  || 0;
+        left += element.offsetLeft || 0;
+        element = element.offsetParent;
+    } while(element);
+
+    return {
+        top: top,
+        left: left
+    };
+};
+
+module.exports = cumulativeOffset;
+
--- src/Pos.js
+++ src/Pos.js
@@ -7,20 +7,59 @@ import './Pos.scss';
 
 import manipulation from 'nlab/manipulation';
 
+import cumulativeOffset from '../cumulativeOffset';
+
 class Pos extends Component {
     componentDidMount() {
-
         const m = document.createElement('div');
-
         manipulation.append(document.body, m);
-
         m.classList.add('mouse');
-
         document.addEventListener('mousemove', this.event = e => {
             m.innerText     = e.clientX + ':' + e.clientY;
             m.style.left    = e.clientX + 20 + 'px';
             m.style.top     = e.clientY + 20 + 'px';
         });
+
+        (() => {
+            let t, m;
+            document.addEventListener('mousedown', this.mousedown = e => {
+
+                t = false;
+
+                if ( ! e.target ) {
+
+                    return;
+                }
+
+                t = e.target;
+
+                t.classList.add('shadow');
+
+                m = document.createElement('div');
+                manipulation.append(document.body, m);
+                m.classList.add('tmp');
+                const c = cumulativeOffset(t);
+                m.innerText     = c.left + ':' + c.top;
+                m.style.left    = e.clientX + 20 + 'px';
+                m.style.top     = e.clientY + 20 + 'px';
+
+
+                log(e.shiftKey)
+            });
+            document.addEventListener('mouseup', this.mouseup = e => {
+
+                if ( ! t ) {
+
+                    return;
+                }
+
+                t.classList.remove('shadow');
+
+                m && manipulation.remove(m);
+                m = false;
+
+            });
+        })();
 
         this.m = m;
     }
@@ -29,19 +68,51 @@ class Pos extends Component {
         this.m = false;
         this.event && document.removeEventListener('mousemove', this.event);
         this.event = false;
+        this.mousedown && document.removeEventListener('mousedown', this.mousedown);
+        this.mousedown = false;
+        this.mouseup && document.removeEventListener('mouseup', this.mouseup);
+        this.mouseup = false;
     }
-
     render() {
         return (
             <div className="pos">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc augue elit, vulputate eget nisl quis, accumsan accumsan lorem. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur pretium magna eu quam accumsan posuere at et urna. Nullam ut turpis condimentum, iaculis magna id, suscipit lorem. In vitae quam convallis lacus lobortis aliquam. Nulla facilisi. Mauris iaculis magna eget mauris pulvinar pharetra. Nullam sit amet dignissim quam. Suspendisse commodo egestas volutpat. Nunc ex elit, auctor eget venenatis at, venenatis vitae ligula. Praesent tristique diam in porttitor ullamcorper. Nullam mattis semper augue, sed dapibus lectus auctor placerat.</p>
+                <p style={{color: 'red'}}>hold 'shift' and click individual elements on the page to see its position</p>
+                <table width="100%">
+                    <tbody>
+                    <tr>
+                        <td width="30%" valign="top">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc augue elit, vulputate eget nisl quis, accumsan accumsan lorem. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur pretium magna eu quam accumsan posuere at et urna. Nullam ut turpis condimentum, iaculis magna id, suscipit lorem. In vitae quam convallis lacus lobortis aliquam. Nulla facilisi. Mauris iaculis magna eget mauris pulvinar pharetra. Nullam sit amet dignissim quam. Suspendisse commodo egestas volutpat. Nunc ex elit, auctor eget venenatis at, venenatis vitae ligula. Praesent tristique diam in porttitor ullamcorper. Nullam mattis semper augue, sed dapibus lectus auctor placerat.</p>
 
-                <p>Pellentesque tempor dapibus nulla ut maximus. Etiam a venenatis mauris. Praesent laoreet aliquam mi, mollis viverra ipsum cursus nec. Praesent nec laoreet ligula. In non metus et nibh dignissim sagittis sed eu felis. Suspendisse ut ornare magna. Vivamus in rhoncus nisi, ut semper ex. Donec varius, lorem nec bibendum dapibus, elit justo congue nunc, non rhoncus est mi non quam. Vestibulum augue lectus, semper quis aliquam et, molestie eu tellus.</p>
+                            <div className="test">plain</div>
+                        </td>
+                        <td width="30%" className="test2" valign="top">
+                            <div className='rel' style={{
+                                top: '20px',
+                                left: '15px`',
+                            }}>
+                                <div className="abs" style={{
+                                    top: '-20px',
+                                    left: '19px',
+                                }}>
+                                    <div className='rel' style={{
+                                        top: '20px',
+                                        left: '15px',
+                                    }}>
+                                        <div className="test" style={{
+                                            top: '24px',
+                                            left: '18px',
+                                        }}>abs in rel, in abs</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </td>
+                        <td width="30%" valign="top">
 
-                <p>Aenean maximus tempor enim id posuere. Etiam euismod egestas magna vel aliquam. Fusce at ullamcorper justo. Vestibulum quis libero quis metus dictum congue quis sit amet lacus. Donec consequat tempor ipsum, at efficitur odio vestibulum ac. Ut hendrerit consequat sem id auctor. Donec quis erat hendrerit felis finibus varius quis at massa. Pellentesque id velit nec velit pharetra bibendum.</p>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
 
-                <p>Sed est massa, imperdiet sit amet luctus nec, ultricies eget nibh. Sed ut pulvinar elit. In ullamcorper massa ligula, ut lacinia massa fringilla sed. Curabitur dolor libero, malesuada imperdiet dui vitae, eleifend rutrum est. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis eu risus lobortis, vestibulum nunc vel, dignissim massa. Nunc ante urna, dignissim id nunc at, pellentesque lacinia enim. Praesent rutrum efficitur leo, et sollicitudin lorem cursus ut. Nunc quis tincidunt nisl.</p>
-                <div className="test">plain</div>
             </div>
         );
     }
